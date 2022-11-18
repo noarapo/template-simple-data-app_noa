1e2d4007b680bb010b151a00af5fcb4371596d37
noarapo <noarapoport12@gmail.com>
2022-11-18 19:24:48 +0200

    ve1

--- index.js
+++ index.js
@@ -8,10 +8,41 @@
 // hint: use this reference later to inject data into your page
 const app = document.getElementById('app');
 
+// Select our HTML select box for sorting our data - (mentioned in html)
+const dataSort = document.getElementById("data-sort");
+// listen for change in state
+dataSort.addEventListener("change", async (event) => {
+  console.log("Data Sort Change");
+  console.log(event.target.value);
+
+  // re-fetching data from our API 
+  const data = await getData();
+  const sortedData = sortData(data, event.target.value);
+  // re-render our app with the sorted data
+  await renderUI(sortedData);
+})
+
+// Here we select our HTML select box for filtering our data
+const dataFilter = document.getElementById("data-filter");
+
+// FILTERS LISTENERS 
+dataFilter.addEventListener("change", async (event) => {
+  console.log("Data Filter Change");
+  console.log(event.target.value);
+  const data = await getData();
+  const filteredData = filterData(data, event.target.value);
+  await renderUI(filteredData);
+})
+
+//---FETCH ---- 
+const API_SOURCE = 'https://api.nobelprize.org/2.1/nobelPrizes';
+
 async function getData() {
-  // write you logic for getting the data from the API here
-  // return your data from this function
-  const data = null;
+  // This function extracts data from noble prize API
+  // return: data from noble prize source
+  let data = null;
+  const response = await fetch(API_SOURCE);
+  data = await response.json();
   return data;
 }
 
@@ -21,19 +52,72 @@ function clearUI() {
   }
 }
 
+
+
+
 async function renderUI(data) {
 
   clearUI();
 
-  // you have your data! add logic here to render it to the UI
-  // notice in the HTML file we call render();
-  const dummyItemElement = Object.assign(document.createElement("div"), { className: "item" })
-  const dummyContentElement = Object.assign(document.createElement("div"), { className: "content" })
-  dummyContentElement.innerHTML = "hey";
-  dummyItemElement.appendChild(dummyContentElement);
-  app.appendChild(dummyItemElement);
+  const { nobelPrizes } = data;
+  const recordTemplate = document.getElementById("data-record-template");
+
+  for (const prize of nobelPrizes){
+  console.log(prize);
+  console.log("here i am");
+  console.log(prize.laureates[0].knownName.en);
+   
+
+    const card = Object.assign(document.createElement("div"),{ className: "item" });
+    const cardContent = Object.assign(document.createElement("div"), { className: "content" });
+    cardContent.innerHTML= ` Category<h2> ${prize.category.en} </h2> \
+                            Name<h3> ${prize.laureates[0].knownName.en} </h3> \
+                            Year<h3> ${prize.awardYear} </h3>  \
+                            Sum <h3> ${prize.prizeAmount} </h3>`
+                          
+    card.appendChild(cardContent);
+    app.appendChild(card);
+   // app.appendChild(recordClone);
+  } 
+  
 }
 
+
+function filterData(data, key) {
+  data.nobelPrizes = data.nobelPrizes.filter((prize) => {
+    if (key === "chemistry") {
+      return prize.category.en === "Chemistry";
+    } else if (key === "physics") {
+      return prize.category.en === "Physics";
+    } else if (key === "literature") {
+      return prize.category.en === "Literature";
+    } else if (key === "peace") {
+      return prize.category.en === "Peace";
+    } else if (key === "physiology-medicine") {
+      return prize.category.en === "Physiology or Medicine";
+    } else {
+      return true;
+    }
+  });
+  return data;
+}
+
+function sortData(data, key) {
+  data.nobelPrizes = data.nobelPrizes.sort((a, b) => {
+    if (a.awardYear > b.awardYear) {
+      return key === "oldest-first" ? 1 : -1;
+    }
+    if (a.awardYear < b.awardYear) {
+      return key === "newest-first" ? -1 : 1;
+    }
+    return 0;
+  })
+  return data;
+}
+
+
+
 const data = await getData();
+console.log(data);
 
 await renderUI(data);
